dd2f93801af3e49a4e2a8eccea55c389536b445e
Zami3l <31670103+Zami3l@users.noreply.github.com>
2020-06-02 00:36:37 +0200

    feat(scripts): modification fonction disk

--- scripts/arch/install.sh
+++ scripts/arch/install.sh
@@ -12,7 +12,7 @@ function setlanguage
     echo "---------------------"
 
     while true; do
-        read -p "What language want you ? " language
+        read -p "What language ? " language
         case $language in 
             [1]* ) loadkeys fr; break;;
             [2]* ) loadkeys us; break;;
@@ -25,14 +25,33 @@ function disk
 {
     echo "-- Disk select --"
     lsblk
-    read -p "What language want you ? " disk
-    echo "---------------------"
+    read -p "What disk ? " disk
+    echo "-----------------"
 
     echo "-- Partionnement type --"
     echo "1 - Boot, Root, no Home, no Swap"
     echo "2 - Boot, Root, Home, no Swap"
     echo "3 - Boot, Root, Home, Swap"
     echo "E - Exit"
+
+    while true; do
+        read -p "What partionnement type ? " language
+        case $language in 
+            [1]* )
+                read -p "Size Root : " $size_root;
+                break;;
+            [2]* )
+                read -p "Size Root : " $size_root;
+                read -p "Size Home : " $size_home;
+                break;;
+            [3]* )
+                read -p "Size Swap : " $size_home;
+                read -p "Size Root : " $size_root;
+                read -p "Size Home : " $size_home;
+                break;;
+            [Ee]* ) exit;;
+        esac
+    done
 
     echo "--------------------"
 }
